ae5e069d141386dbc134d6e459afafaed4459b38
Samuelnoah45 <samuelnoah668@gmail.com>
2025-07-23 10:41:57 +0300

    fix: move test file to test folder

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,18 +1,18 @@
-const mathOperations = require('./calculator');
- 
+const mathOperations = require("../calculator");
+
 describe("Calculator tests", () => {
-  test('adding 1 + 2 should return 3', () => {
-    var result = mathOperations.sum(1,2);
+  test("adding 1 + 2 should return 3", () => {
+    var result = mathOperations.sum(1, 2);
     expect(result).toBe(3);
   });
 
   test("subtracting 2 from 10 should return 8", () => {
-    var result = mathOperations.diff(10,2);
+    var result = mathOperations.diff(10, 2);
     expect(result).toBe(8);
   });
-   
+
   test("multiplying 2 and 4 should return 8", () => {
-    var result = mathOperations.product(2,4);
+    var result = mathOperations.product(2, 4);
     expect(result).toBe(8);
   });
-})
+});
